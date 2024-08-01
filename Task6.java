08f2ce1f1fb4d92fb4445e446ab2909093622329
Horlakz <officialhorlakz@gmail.com>
2024-08-01 16:43:50 +0100

    removed comments from code

--- Task6.java
+++ Task6.java
@@ -1,30 +1,33 @@
+
+import java.util.Scanner;
+
 public class Task6 {
 
     private static String name = "Horlakz";
     
     public static void main(String[] args) {
-        // Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        // System.out.println("number of times to run the for loop: ");
-        // int userTimes = input.nextInt();
+        System.out.println("number of times to run the for loop: ");
+        int userTimes = input.nextInt();
 
-        // int times = 0;
-        // while (times < userTimes) {
-        //     times++;
-        //     System.out.printf("Count = %s \n", times);
-        // }
+        int times = 0;
+        while (times < userTimes) {
+            times++;
+            System.out.printf("Count = %s \n", times);
+        }
 
 
-        // for (int i = 0; i < userTimes ; i++) {
-        //     System.out.printf("Count = %s \n", i + 1);
-        // }
+        for (int i = 0; i < userTimes ; i++) {
+            System.out.printf("Count = %s \n", i + 1);
+        }
 
         
         int result = publicFunction();
 
         System.out.println(result);
 
-        // input.close();
+        input.close();
     }
 
     public static int publicFunction() {
