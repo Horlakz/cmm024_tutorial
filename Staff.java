1081745bb66b8d56edd740c070e3627a77cfac93
Horlakz <officialhorlakz@gmail.com>
2024-08-03 14:55:02 +0100

    fix: errors in staff and stafftest files

--- Staff.java
+++ Staff.java
@@ -1,21 +1,21 @@
 public class Staff extends Person {
-    private String staffID;
-    private String NI;
-
+    private final String staffID;
+    private final String NI;
 
     public Staff(String name, int age, String currentAddress, String staffID, String NI) {
-        super(name, age, currentAddress);
+        super(name, currentAddress, age);
 
         this.staffID = staffID;
         this.NI = NI;
     }
 
+    @Override
     public String toString() {
-        return "name" + this.name +
-             "age" + this.age +
-             "current address" + this.currentAddress + 
-             "staff id" + this.staffID + 
-             "ni" + this.NI
+        return " name " + this.getName() +
+                " age " + this.getAge() +
+                " current address " + this.getCurrentAddress() +
+                " staff id " + this.staffID +
+                " ni " + this.NI;
     }
 
     public String getStaffID() {
--- StaffTest.java
+++ StaffTest.java
@@ -2,12 +2,12 @@ public class StaffTest {
 
     public static void main(String[] args) {
         Staff staff = new Staff("Horlakz", 20, "unilag", "2", "12");
-        Person person = new Person("Dammie", 23, "scotland");
+        Person person = new Person("Dammie", "Scotland", 23);
 
         if (staff instanceof Staff) {
             System.out.println(staff.getStaffID());
         }
 
-        System.out.println(staff.toString());
+        System.out.println(staff.toString() + "\n" + person.toString());
     }
 }
